8394f12d9879b53fbc6f258bef99e0d6570af11c
Anchit Seth <anchitseth2809@gmail.com>
2019-10-01 08:58:53 +0800

    edit cors

--- src/main/java/com/nus/iss/eatngreet/user/config/BeanConfig.java
+++ src/main/java/com/nus/iss/eatngreet/user/config/BeanConfig.java
@@ -1,6 +1,12 @@
 package com.nus.iss.eatngreet.user.config;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +16,19 @@ public class BeanConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("http://eat-n-greet1.azurewebsites.net");
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200");
 	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("*"));
+	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+	        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+	        configuration.setAllowCredentials(true);
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 
 }
